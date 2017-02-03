741bd454025088664dc855a6221bf5a08a4fc768
BaptisteGaitet <baptiste.gaitet@gmail.com>
2017-02-03 11:12:28 +0100

    Creates Animation class

---
+++ Animation.cpp
@@ -0,0 +1,48 @@
+#include "Animation.h"
+
+
+Animation::Animation(std::vector<Frame> _frames, bool _looped)
+{
+	frames = _frames;
+	looped = _looped;
+	currentFrameIndex = 0;
+}
+
+void Animation::update()
+{
+	frames.at(currentFrameIndex).update();
+
+	if (frames.at(currentFrameIndex).finished())
+	{
+		if (currentFrameIndex < (frames.size() - 1))
+		{
+			frames.at(currentFrameIndex).reset();
+			currentFrameIndex++;
+		}
+		else if (looped)
+		{
+			currentFrameIndex = 0;
+		}
+	}
+}
+
+int Animation::getCurrentFrameNumber()
+{
+	return frames.at(currentFrameIndex).getNumber();
+}
+
+bool Animation::finished()
+{
+	bool res = false;
+
+	if (!looped && currentFrameIndex >= frames.size() - 1)
+	{
+		res = true;
+	}
+
+	return res;
+}
+
+Animation::~Animation()
+{
+}
---
+++ Animation.h
@@ -0,0 +1,27 @@
+#ifndef ANIMATION_H
+#define ANIMATION_H
+
+#include <vector>
+#include "Frame.h"
+
+class Animation
+{
+private:
+	std::string id;
+	std::vector<Frame> frames;
+	int currentFrameIndex;
+	bool looped;
+
+public:
+	Animation(std::vector<Frame> _frames, bool _looped);
+
+	void update();
+	int getCurrentFrameNumber();
+	bool finished();
+
+	~Animation();
+};
+
+#endif
+
+

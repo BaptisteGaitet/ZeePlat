55f919615fa588ea6663ba80179c5527997b6fe9
BaptisteGaitet <baptiste.gaitet@gmail.com>
2017-02-05 16:34:31 +0100

    Creates TileMap class

---
+++ TileMap.cpp
@@ -0,0 +1,45 @@
+#include "TileMap.h"
+
+TileMap::TileMap(sf::FloatRect _bounds, sf::Vector2i _size, sf::Vector2i _tileSize, std::string _path, int* _indexes, bool* _hitboxes)
+{
+	size = _size;
+	sf::Vector2f renderTileSize = sf::Vector2f(_bounds.width / _size.x, _bounds.height / _size.y);
+	for (int i = 0; i < _size.x; i++)
+	{
+		for (int j = 0; j < _size.y; j++)
+		{
+			int tileIndex = _indexes[j * _size.x + i];
+			sf::Vector2f tilePosition = sf::Vector2f(i * renderTileSize.x + _bounds.left, j * renderTileSize.y + _bounds.top);
+
+			Tile tile = Tile(tileIndex, tilePosition, renderTileSize, _tileSize, _path, _hitboxes[j * _size.x + i]);
+			tiles.push_back(tile);
+		}
+	}
+}
+
+void TileMap::draw(sf::RenderWindow* window)
+{
+	for (int i = 0; i < tiles.size(); i++)
+	{
+		tiles.at(i).draw(window);
+	}
+}
+
+void TileMap::separate(Body* _body)
+{
+	for (int i = 0; i < _body->getHitboxes().size(); i++)
+	{
+		for (int j = 0; j < tiles.size(); j++)
+		{
+			sf::Vector2f overlap = tiles.at(j).getHitbox()->overlap(_body->getHitboxes().at(i));
+
+			tiles.at(j).getHitbox()->separate(_body->getHitboxes().at(i), overlap);
+
+			_body->snapAllToHitbox(i);
+		}
+	}
+}
+
+TileMap::~TileMap()
+{
+}
---
+++ TileMap.h
@@ -0,0 +1,22 @@
+#ifndef TILE_MAP_H
+#define TILE_MAP_H
+
+#include <vector>
+#include "Tile.h"
+#include "Body.h"
+
+class TileMap
+{
+private:
+	std::vector<Tile> tiles;
+	sf::Vector2i size;
+public:
+	TileMap(sf::FloatRect _bounds, sf::Vector2i _size, sf::Vector2i _tileSize, std::string _path, int* _indexes, bool* _hitboxes);
+
+	void draw(sf::RenderWindow* window);
+	void separate(Body* _body);
+
+	~TileMap();
+};
+
+#endif

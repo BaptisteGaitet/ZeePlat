f729fd25d86ccee3a7c04c54bc0592bece3db03f
BaptisteGaitet <baptiste.gaitet@gmail.com>
2017-02-06 16:23:14 +0100

    Creates GameState class

---
+++ GameState.cpp
@@ -0,0 +1,26 @@
+#include "GameState.h"
+
+
+GameState::GameState() : State("game")
+{
+	player = Player(sf::Vector2f(100, 100));
+	levels.insert(std::pair<std::string, Level>("test",LevelLoader::loadLevel("test.txt", "test8x8.png")));
+	currentLevel = "test";
+}
+
+void GameState::update()
+{
+	player.update();
+	levels.at(currentLevel).separate(player.getBody());
+}
+
+void GameState::draw(sf::RenderWindow* window)
+{
+	levels.at(currentLevel).drawBackground(window);
+	player.draw(window);
+	levels.at(currentLevel).drawForeground(window);
+}
+
+GameState::~GameState()
+{
+}
---
+++ GameState.h
@@ -0,0 +1,25 @@
+#ifndef GAME_STATE_H
+#define GAME_STATE_H
+
+#include "State.h"
+#include "Player.h"
+#include "Level.h"
+#include "LevelLoader.h"
+
+class GameState :
+	public State
+{
+private:
+	Player player;
+	std::map<std::string, Level> levels;
+	std::string currentLevel;
+public:
+	GameState();
+
+	void update();
+	void draw(sf::RenderWindow* window);
+
+	~GameState();
+};
+
+#endif

56ab3a246a0b1dbd8e547adbbec4784454739cef
BaptisteGaitet <baptiste.gaitet@gmail.com>
2017-02-06 14:49:52 +0100

    Adds tests for levels

--- main.cpp
+++ main.cpp
@@ -5,14 +5,17 @@
 #include "AnimatedImage.h"
 #include "Hitbox.h"
 #include "Body.h"
-#include "TileMap.h"
+#include "Level.h"
+#include "LevelLoader.h"
 #include "Player.h"
 
 int main()
 {
-	Player player = Player(sf::Vector2f(64,64));
+	Player player = Player(sf::Vector2f(100,100));
 
-	int indexes[20 * 16] = {
+	Level lvl = LevelLoader::loadLevel("test.txt", "test8x8.png");
+
+	/*int indexes[20 * 16] = {
 		3, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 3,
 		4, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4,
 		4, 3, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 3, 4,
@@ -48,12 +51,12 @@ int main()
 		true, true, false, false, false, false, false, false, false, false, false, false, false, false, false, true, true, true, true, true,
 		true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true,
 		true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true,
-	};
+	};*/
 
 	sf::RenderWindow window(sf::VideoMode(800, 600), "ZeePlat");
 	window.setFramerateLimit(60);
 
-	TileMap map = TileMap(sf::FloatRect(0, 0, 20*32, 16*32), sf::Vector2i(20, 16), sf::Vector2i(8, 8), "test8x8.png", indexes, hitboxes);
+	//TileMap map = TileMap(sf::FloatRect(0, 0, 20*32, 16*32), sf::Vector2i(20, 16), sf::Vector2i(8, 8), "test8x8.png", indexes, hitboxes);
 
 	sf::RectangleShape hb = sf::RectangleShape(sf::Vector2f(16, 16));
 	hb.setFillColor(sf::Color(100, 200, 100, 128));
@@ -69,16 +72,18 @@ int main()
 
 		player.update();
 		
-		map.separate(player.getBody());
+		lvl.separate(player.getBody());
 
 		//hb.setPosition(player.getHitbox()->getPosition());
 
 		window.clear();
 
-		map.draw(&window);
+		//map.draw(&window);
+		lvl.drawBackground(&window);
 
 		player.draw(&window);
 
+		lvl.drawForeground(&window);
 		//window.draw(hb);
 
 		window.display();

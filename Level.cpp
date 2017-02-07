18af9adbb64087366c4f849f52ae6885b3c7766e
BaptisteGaitet <baptiste.gaitet@gmail.com>
2017-02-07 16:42:42 +0100

    Adds WarpZone to Levels

--- Level.cpp
+++ Level.cpp
@@ -40,6 +40,29 @@ void Level::separate(Body* _body)
 	mainLayer->separate(_body);
 }
 
+void Level::setWarpZones(std::vector<WarpZone> _warpZones)
+{
+	for (int i = 0; i < _warpZones.size(); i++)
+	{
+		warpZones.push_back(_warpZones.at(i));
+	}
+}
+
+WarpZone Level::getOvelapingWarpZone(sf::FloatRect _rect)
+{
+	WarpZone res = WarpZone(sf::FloatRect(0, 0, 0, 0), "", sf::Vector2f(0, 0));
+
+	for (int i = 0; i < warpZones.size(); i++)
+	{
+		if (warpZones.at(i).overlap(_rect))
+		{
+			res = warpZones.at(i);
+		}
+	}
+
+	return res;
+}
+
 Level::~Level()
 {
 }
--- Level.h
+++ Level.h
@@ -6,6 +6,7 @@
 #include "TileMap.h"
 #include "Image.h"
 #include "Entity.h"
+#include "WarpZone.h"
 
 class Level
 {
@@ -15,6 +16,7 @@ private:
 	TileMap* mainLayer;
 	TileMap* frontLayer;
 	std::vector<Entity*> entities;
+	std::vector<WarpZone> warpZones;
 public:
 	Level(TileMap* _backLayer, TileMap* _mainLayer, TileMap* _frontLayer, std::vector<Entity*> _entities, sf::Color _backgroundColor);
 
@@ -22,6 +24,9 @@ public:
 	void drawBackground(sf::RenderWindow* window);
 	void drawForeground(sf::RenderWindow* window);
 	void separate(Body* _body);
+
+	void setWarpZones(std::vector<WarpZone> _warpZones);
+	WarpZone getOvelapingWarpZone(sf::FloatRect _rect);
 
 	~Level();
 };

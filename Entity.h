a04edb31ef43ac5b9e31b96b435ac7f9d8dd9840
BaptisteGaitet <baptiste.gaitet@gmail.com>
2017-02-11 13:10:58 +0100

    Adds particle manager adress to update call

--- Entity.h
+++ Entity.h
@@ -4,6 +4,7 @@
 #include "AnimatedImage.h"
 #include "Body.h"
 #include "Hitbox.h"
+#include "ParticleManager.h"
 
 class Entity
 {
@@ -16,7 +17,7 @@ public:
 	Entity(sf::Vector2f _position, sf::Vector2f _size, std::string _path, int _spriteRows, int _spriteColumns);
 	Entity(Body _body, AnimatedImage _animatedImage);
 
-	virtual void update() = 0;
+	virtual void update(ParticleManager* _partMngr) = 0;
 	void draw(sf::RenderWindow* window);
 	Body* getBody();
 	sf::Vector2f getCenter();
